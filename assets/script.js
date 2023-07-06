5e3cf6841980f3a4160cc6715d5ebdc247e119bb
unknown <samtisme@gmail.com>
2023-07-06 03:29:04 -0700

    added functions for button clicks and results tallies, fixed bug with page not loading javascript by changing the structure of the functions formatting. this one took quite some time

--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,133 @@
-var basketballQuiz = [
-    {
-        question: "Who has scored the most points ever in an nba career",
-        choices: ["Lebron James", "Micheal Jordan", "Shaquille Oneal", "Kareem Abdul Jabbar"],
-        correctAnswer: 0,
-        userAnswer: null
-    },
-    {
-        question: "Which Nba player has scored the most 3 point goals ever in a single game and a single season",
-        choices: ["Steve Nash", "Kobe Bryant", "Kevin Durant", "Stephen Curry"],
-        correctAnswer: 3,
-        userAnswer: null
-    },
-    {
-        question: "Which Nba team has the most Nba championship wins of all time",
-        choices: ["LA Lakers", "GS Warriors", "HOU Rockets", "BOS Celtics"],
-        correctAnswer: 3,
-        userAnswer: null
-    },
-    {
-        question: "Which former Nba Champion holds the record for the most points scored in a single Arena",
-        choices: ["Larry Bird", "Magic Johnson", "Kobe Bryant", "Bill Russel"],
-        correctAnswer: 2,
-        userAnswer: null
-    },
-];
-
-const quizContainer = document.getElementById("test");
-const timerContainer = document.getElementById("timer");
+const quizContainer = document.getElementById("quiz-container");
+const timerElement = document.getElementById("timer");
 const startButton = document.getElementById("start-button");
+const resultsContainer = document.getElementById("results-container");
+
+const quizData = [
+  {
+    question: "Who has scored the most points ever in an NBA career?",
+    choices: ["LeBron James", "Michael Jordan", "Shaquille O'Neal", "Kareem Abdul-Jabbar"],
+    correctAnswer: 0,
+    userAnswer: null
+  },
+  {
+    question: "Which NBA player has scored the most 3-point goals ever in a single game and a single season?",
+    choices: ["Steve Nash", "Kobe Bryant", "Kevin Durant", "Stephen Curry"],
+    correctAnswer: 3,
+    userAnswer: null
+  },
+  {
+    question: "Which NBA team has the most NBA championship wins of all time?",
+    choices: ["LA Lakers", "GS Warriors", "HOU Rockets", "BOS Celtics"],
+    correctAnswer: 3,
+    userAnswer: null
+  },
+  {
+    question: "Which former NBA champion holds the record for the most points scored in a single arena?",
+    choices: ["Larry Bird", "Magic Johnson", "Kobe Bryant", "Bill Russell"],
+    correctAnswer: 2,
+    userAnswer: null
+  },
+];
 
 const totalTime = 30;
 let timeRemaining = totalTime;
 let timerInterval;
+let currentQuestionIndex = 0;
 
 function startTimer() {
-    timerInterval = setInterval(updateTimer, 1000);
+  timerInterval = setInterval(updateTimer, 1000);
 }
 
 function updateTimer() {
-    if (timeRemaining > 0) {
-        timeRemaining--;
-        timerElement.textContent = formatTime(timeRemaining);
-    } else {
-        clearInterval(timerInterval);
-        endTest();
-    }
+  if (timeRemaining > 0) {
+    timeRemaining--;
+    timerElement.textContent = formatTime(timeRemaining);
+  } else {
+    clearInterval(timerInterval);
+    endTest();
+  }
 }
 
 function formatTime(seconds) {
-    const minutes = Math.floor(seconds / 30);
-    const secondsRemaining = seconds % 30;
-    return `${padZero(minutes)}:${padZero(secondsRemaining)}`;
+  const minutes = Math.floor(seconds / 60);
+  const secondsRemaining = seconds % 60;
+  return `${padZero(minutes)}:${padZero(secondsRemaining)}`;
 }
+
+function padZero(number) {
+  return number.toString().padStart(2, "0");
+}
+
+function endTest() {
+  let correctAnswers = 0;
+  let wrongAnswers = 0;
+
+  for (const question of quizData) {
+    if (question.userAnswer === question.correctAnswer) {
+      correctAnswers++;
+    } else {
+      wrongAnswers++;
+    }
+  }
+
+  resultsContainer.innerHTML = `
+    <h2>Quiz Results</h2>
+    <p>Correct Answers: ${correctAnswers}</p>
+    <p>Wrong Answers: ${wrongAnswers}</p>
+  `;
+
+  quizContainer.style.display = "none";
+  resultsContainer.style.display = "block";
+}
+
+function loadQuestion() {
+  const currentQuestion = quizData[currentQuestionIndex];
+  const questionElement = document.getElementById("question");
+  const choicesContainer = document.getElementsByClassName("choice");
+
+  questionElement.textContent = currentQuestion.question;
+
+  for (let i = 0; i < choicesContainer.length; i++) {
+    choicesContainer[i].textContent = currentQuestion.choices[i];
+    choicesContainer[i].setAttribute("data-index", i);
+  }
+}
+
+function handleChoiceClick(event) {
+  const selectedChoice = event.target;
+  const selectedChoiceIndex = parseInt(selectedChoice.getAttribute("data-index"));
+
+  quizData[currentQuestionIndex].userAnswer = selectedChoiceIndex;
+
+  handleNextButtonClick();
+}
+
+function handleNextButtonClick() {
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex >= quizData.length) {
+    clearInterval(timerInterval);
+    endTest();
+    return;
+  }
+
+  loadQuestion();
+}
+
+function initializeQuiz() {
+  startButton.addEventListener("click", handleStartButtonClick);
+  const choicesContainer = document.getElementsByClassName("choice");
+
+  for (let i = 0; i < choicesContainer.length; i++) {
+    choicesContainer[i].addEventListener("click", handleChoiceClick);
+  }
+}
+
+function handleStartButtonClick() {
+  startTimer();
+  loadQuestion();
+  startButton.disabled = true;
+}
+
+initializeQuiz();
